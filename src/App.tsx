50bab8a3ba76d2f733df07f3bec5f131616a5461
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-09-11 22:10:14 -0300

    feat:header initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>hello</h1>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center  justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
